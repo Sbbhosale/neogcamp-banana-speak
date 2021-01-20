14f68ae32e300bcc687d07e878739b391cfff463
Sbbhosale <77296447+Sbbhosale@users.noreply.github.com>
2021-01-20 15:30:41 +0530

    feat : added text input
    added wiring for text input

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+
+console.log(textInput)
 
 function addclickhandler()
 {
-    console.log("clicked")
+    console.log("clicked");
+    console.log( "Input : ", textInput.value);
 }
-
-
 btnTranslate.addEventListener("click", addclickhandler);
