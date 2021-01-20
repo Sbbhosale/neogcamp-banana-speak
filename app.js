c8ba346ebeec4f42636454ae5de6ae24702773d9
Sbbhosale <77296447+Sbbhosale@users.noreply.github.com>
2021-01-20 14:25:11 +0530

    feat : prompt and alert
    worked on prompt and alert

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("It's working.");
+//alert : used for showing output
+alert("This site is working..")
+
+
+//prompt : used for taking user input
+prompt("enter you name")
+//how can we use prompt
+
+
+
+var userName = prompt("enter you name");
+alert("This site is Loading " + userName);
